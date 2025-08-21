88318d93af336788b093b3cdc95f2b7a6ac52849
Adesoji Ajijolaoluwa <ajeeaidev@gmail.com>
2025-08-21 01:08:53 +0100

    modified tasks

---
+++ Desktop/PythonLearn/First.py
@@ -0,0 +1,98 @@
+# # # # # print("Hello, World!")
+
+# # # # n = "Ajee"
+# # # # # print("My name is", n)
+
+# # # # n = "David"
+# # # # # print("My name is", n)
+
+# # # # naME = "Aj"
+# # # # NAme = "Aji"
+# # # # na_me = "Dave"
+# # # # _niv = "Bffd"
+
+
+# # # # name1 = "jh8fd"
+
+
+# # # # print(naME)
+# # # # print(NAme)
+# # # # print(na_me)
+# # # # print(_niv)
+# # # # print(name1)
+
+
+# # # # # print(n/2)
+
+# # # # # print(n * 2)
+
+
+# # # tens = 20
+
+# # # print(int(tens/2))
+# # # print("//")
+# # # print(20 * 2)
+# # # print("//")
+# # # print(20 + 2)
+
+# # # print("//")
+# # # print(20 - 2)
+# # # print("//")
+# # # print(20 // 3)
+# # # print("//")
+# # # print( 20 % 7)
+# # # print("//")
+# # # print(20 ** 2)
+# # # print("//")
+
+# # # print(100 ** 0.5)
+
+
+
+
+
+
+# # from math import ceil, floor
+
+
+# # float1 = 15.6
+# # print(float1)
+
+# # print(float1/15)
+# # print(round(float1/15))
+# # print(ceil(float1/15))
+# # print(floor(15.9))
+# # print(round(15.7))
+# # print(round(15.4))
+
+# print(5 != 9)
+
+
+# gen = False
+# print(type(gen))
+# IF = "IFUD"
+# print(IF)
+
+# name = "Aisha"
+# age = 25
+# price = 10000
+# print("Name:", name)
+# print("Name:" + " " + name)
+# print(f"Name: {name}")
+# print("Age:", age)
+# print(f"Price: {price}")
+# print(f"Price: {price:,}")
+
+# first_name = "Ada"
+# last_name = "Lovelace"
+# print("Full name: " + first_name + " " + last_name)
+# print("Line1\nLine2")
+# print("Iem1Item2")
+# print("Item1\tItem2")
+
+# # print("He said, "Hello"")
+
+# print('He said, "Hello" ')
+# print("He said, 'Hello' ")
+# print("He said, \"Hello\"")
+# print('It\'s Python!')
